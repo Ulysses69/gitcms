87a59f447cdd2509dde0d4c67d99d795199b0e60
Ulysses69 <steven@poppymedia.co.uk>
2014-10-09 10:04:50 +0100

    Added note about URL format in sidebar.

--- wolf/plugins/redirector/views/sidebar.php
+++ wolf/plugins/redirector/views/sidebar.php
@@ -18,6 +18,7 @@
 <h2><?php echo __('About');?></h2>
 <p>Pages can be re-directed by simply specifying the page URL to be re-directed from and the page URL it is to be re-directed to.</p>
 <!-- <p>Set the duration of the re-direct using the status option. If using temporary re-direct, be sure to remove the re-direct when it is no longer needed or set to permanent if circumstances require.</p> -->
+<p>Care should be taken with format of entered URLs</p>
 </div>
 
 <?php include $_SERVER{'DOCUMENT_ROOT'}.URL_PUBLIC.'wolf/plugins/_htaccess/views/sidebar.php'; ?>
