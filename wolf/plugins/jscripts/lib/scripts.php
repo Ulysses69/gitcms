53d8c8490ff6133323fe47f1475de2e360abcad4
Ulysses69 <steven@poppymedia.co.uk>
2014-10-13 15:14:39 +0100

    Added check for DOM and HTML5 Savvy browsers, to ensure only newer browsers run javascript.

--- wolf/plugins/jscripts/lib/scripts.php
+++ wolf/plugins/jscripts/lib/scripts.php
@@ -1,42 +1,46 @@
-/* Add js class to document */
-document.getElementsByTagName('body')[0].className+=' js';
+/* Check if browser is DOM and HTML5 Savvy */
+if ('querySelector' in document && 'addEventListener' in window) {
 
-var links = document.links;
-var inputs = document.getElementsByTagName('input');
-
-function hasClass(ele,cls) {
-	return ele.className.match(new RegExp('(\\s|^)'+cls+'(\\s|$)'));
-};
-
-function addClass(ele,cls) {
-	if (!this.hasClass(ele,cls)) ele.className += " "+cls;
-};
-
-function removeClass(ele,cls){
-	if (hasClass(ele,cls)) {
-		var reg = new RegExp('(\\s|^)'+cls+'(\\s|$)');
-		ele.className=ele.className.replace(reg,' ');
-	}
-};
-
-function assignClickEvent(){
-	for(var i =0; i < links.length; i++){
-		links[i].onmousedown = findClick;
-		//alert(links[i].nodeName);
-	}
-	for(var i =0; i < inputs.length; i++){
-		inputs[i].onmousedown = findClick;
-		//alert(inputs[i].nodeName);
-	}
-};
-
-function findClick(e) {
-	for(var i=0; i < links.length; i++){
-		links[i] == this ? addClass(links[i],'click') : removeClass(links[i],'click');
-	}
-	for(var i=0; i < inputs.length; i++){
-		inputs[i] == this ? addClass(inputs[i],'click') : removeClass(inputs[i],'click');
-	}
-};
-
-assignClickEvent();
+	/* Add js class to document */
+	document.getElementsByTagName('body')[0].className+=' js';
+	
+	var links = document.links;
+	var inputs = document.getElementsByTagName('input');
+	
+	function hasClass(ele,cls) {
+		return ele.className.match(new RegExp('(\\s|^)'+cls+'(\\s|$)'));
+	};
+	
+	function addClass(ele,cls) {
+		if (!this.hasClass(ele,cls)) ele.className += " "+cls;
+	};
+	
+	function removeClass(ele,cls){
+		if (hasClass(ele,cls)) {
+			var reg = new RegExp('(\\s|^)'+cls+'(\\s|$)');
+			ele.className=ele.className.replace(reg,' ');
+		}
+	};
+	
+	function assignClickEvent(){
+		for(var i =0; i < links.length; i++){
+			links[i].onmousedown = findClick;
+			//alert(links[i].nodeName);
+		}
+		for(var i =0; i < inputs.length; i++){
+			inputs[i].onmousedown = findClick;
+			//alert(inputs[i].nodeName);
+		}
+	};
+	
+	function findClick(e) {
+		for(var i=0; i < links.length; i++){
+			links[i] == this ? addClass(links[i],'click') : removeClass(links[i],'click');
+		}
+		for(var i=0; i < inputs.length; i++){
+			inputs[i] == this ? addClass(inputs[i],'click') : removeClass(inputs[i],'click');
+		}
+	};
+	
+	assignClickEvent();
+}
