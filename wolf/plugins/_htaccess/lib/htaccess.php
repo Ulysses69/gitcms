cf4e9b1b06896aa5e6c8f3e9e33b56ddf02fcf19
Ulysses69 <anything69>
2014-10-14 08:50:06 +0100

    Introduced mod_deflate to replace mod_gzip

--- wolf/plugins/_htaccess/lib/htaccess.php
+++ wolf/plugins/_htaccess/lib/htaccess.php
@@ -273,19 +273,19 @@ Header always set Expires "Thu, 01 Jan 1970 00:00:00 GMT" env=nocache
 /* GZip Compression | Disabled by default */
 $gzip = true;
 if($gzip == true){ ?>
-#<IfModule mod_deflate.c>
-#AddOutputFilterByType DEFLATE text/html text/plain text/xml application/xml application/xhtml+xml text/css text/javascript application/javascript application/x-javascript
-#</IfModule>
-<ifModule mod_gzip.c>
-mod_gzip_on Yes
-mod_gzip_dechunk Yes
-mod_gzip_item_include file .(html?|txt|xml|css|js|php)$
-mod_gzip_item_include handler ^cgi-script$
-mod_gzip_item_include mime ^text/.*
-mod_gzip_item_include mime ^application/x-javascript.*
-mod_gzip_item_exclude mime ^image/.*
-mod_gzip_item_exclude rspheader ^Content-Encoding:.*gzip.*
-</ifModule>
+<IfModule mod_deflate.c>
+AddOutputFilterByType DEFLATE text/html text/plain text/xml application/xml application/xhtml+xml text/css text/javascript application/javascript application/x-javascript
+</IfModule>
+#<ifModule mod_gzip.c>
+#mod_gzip_on Yes
+#mod_gzip_dechunk Yes
+#mod_gzip_item_include file .(html?|txt|xml|css|js|php)$
+#mod_gzip_item_include handler ^cgi-script$
+#mod_gzip_item_include mime ^text/.*
+#mod_gzip_item_include mime ^application/x-javascript.*
+#mod_gzip_item_exclude mime ^image/.*
+#mod_gzip_item_exclude rspheader ^Content-Encoding:.*gzip.*
+#</ifModule>
 <?php } ?>
 
 
