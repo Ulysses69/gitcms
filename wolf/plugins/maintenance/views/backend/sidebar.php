957a953ff2a4130f7e4c3b5f978b2f01ef0bd6e8
Ulysses69 <steven@poppymedia.co.uk>
2014-10-17 11:54:56 +0100

    Added maintenance access IP log to include mockups access.

--- wolf/plugins/maintenance/views/backend/sidebar.php
+++ wolf/plugins/maintenance/views/backend/sidebar.php
@@ -19,37 +19,52 @@
 
 
 
-<?php
-$log_file = $_SERVER["DOCUMENT_ROOT"].'/../admin-log.txt';
-if(filesize($log_file) > 0){ ?>
 
-<div class="box warning logins">
-<h2><?php echo __('Logged IPs');?></h2>
-<p>Failed attempts at admin access.</p>
 
 <?php
 //echo "<html><body><table>\n\n";
-echo '<ul>';
-$f = fopen($log_file, "r");
-$used_ips = array();
-while (($line = fgetcsv($f)) !== false) {
-	//echo "<tr>";
-	foreach ($line as $cell) {
-		//echo "<td>" . htmlspecialchars($cell) . "</td>";
+function logList($log_file){
+	echo '<ul>';
+	$f = fopen($log_file, "r");
+	$used_ips = array();
+	while (($line = fgetcsv($f)) !== false) {
+		//echo "<tr>";
+		foreach ($line as $cell) {
+			//echo "<td>" . htmlspecialchars($cell) . "</td>";
+		}
+		//echo "</tr>\n";
+		$date = $line[0];
+		$time = $line[1];
+		$ip = $line[2];
+		if(!in_array($ip, $used_ips)){
+			echo '<li><p><b>' . $ip . '</b><span> on ' . $date . ' at ' . $time . '<span></p></li>';
+		}
+		$used_ips[] = $ip;
 	}
-	//echo "</tr>\n";
-	$date = $line[0];
-	$time = $line[1];
-	$ip = $line[2];
-	if(!in_array($ip, $used_ips)){
-		echo '<li><p><b>' . $ip . '</b><span> on ' . $date . ' at ' . $time . '<span></p></li>';
-	}
-	$used_ips[] = $ip;
+	fclose($f);
+	echo '</ul>';
 }
-fclose($f);
-echo '</ul>';
 //echo "\n</table></body></html>";
 ?>
-</div>
 
+
+<?php
+$log_file = $_SERVER["DOCUMENT_ROOT"].'/../admin-log.txt';
+$mockup_file = $_SERVER["DOCUMENT_ROOT"].'/../mockup-log.txt';
+?>
+
+
+<div class="box warning logins">
+<h2><?php echo __('Logged IPs');?></h2>
+
+<?php if(filesize($log_file) > 0){ ?>
+<p>Failed attempts at admin access.</p>
+<?php logList($log_file); ?>
 <?php } ?>
+
+<?php if(filesize($mockup_file) > 0){ ?>
+<p>Access to mockups.</p>
+<?php logList($mockup_file); ?>
+<?php } ?>
+
+</div>
