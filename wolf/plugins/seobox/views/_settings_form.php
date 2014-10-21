bf5d3ec99f3a4a92d94e746df57f860503ef0b9b
Ulysses69 <steven@poppymedia.co.uk>
2014-10-21 10:55:12 +0100

    SEO and Robots handling is now prominantly positioned on SEO page.

--- wolf/plugins/seobox/views/_settings_form.php
+++ wolf/plugins/seobox/views/_settings_form.php
@@ -28,6 +28,113 @@ $clientanalyticsversion = Plugin::getSetting('clientanalyticsversion', 'seobox')
 <br />
 
 	<fieldset style="padding: 0.5em;">
+		<legend style="padding: 0em 0.5em 0em 0.5em; font-weight: bold;"><?php echo __('Search Engines'); ?></legend>
+		<table class="fieldset" cellpadding="0" cellspacing="0" border="0">
+			<tr>
+				<td class="label"><label for="bots"><?php echo __('Robots'); ?></label></td>
+				<td class="field">
+				<input type="checkbox" name="bots" id="bots" value="allow" class="checkbox"<?php if($bots == "allow"){echo " checked";}?>/>
+				</td>
+				<td class="help"><?php echo __('Allow or Disallow. Check to allow robots to index pages.');?></td>
+			</tr>
+		</table>
+	</fieldset>
+
+
+	<fieldset style="padding: 0.5em;">
+		<legend style="padding: 0em 0.5em 0em 0.5em; font-weight: bold;"><?php echo __('Analytics'); ?></legend>
+		<table class="fieldset" cellpadding="0" cellspacing="0" border="0">
+			<tr>
+				<td class="label"><label for="clientanalyticsstatus"><?php echo __('Enable'); ?></label></td>
+				<td class="field">
+				<input type="checkbox" name="clientanalyticsstatus" id="clientanalyticsstatus" value="on" class="checkbox"<?php if($clientanalyticsstatus == "on"){echo " checked";}?>/>
+				</td>
+				<td class="help"><?php echo __('Enable or Disable');?></td>
+			</tr>
+
+			<tr>
+				<td class="label"><label for="clientanalyticsversion"><?php echo __('Version'); ?></label></td>
+				<td class="field">
+				<select name="clientanalyticsversion" id="clientanalyticsversion">
+				<?php
+				$clientanalyticsversion_array = array(
+				array ('Classic', 'classic'),
+				array ('Universal (NEW)', 'universal'));
+				foreach($clientanalyticsversion_array as $subarray) {
+					list($text, $val) = $subarray;
+					if($val == $clientanalyticsversion){
+						echo "<option value=\"".str_replace('"',"'",$val)."\" selected>$text</option>";
+					} else {
+						echo "<option value=\"".str_replace('"',"'",$val)."\">$text</option>";
+					}
+				}
+				?>
+				</select>
+				</td>
+				<td class="help"><?php echo __('Analytics method.');?></td>
+			</tr>
+
+			<tr>
+				<td class="label"><label for="clientanalyticslinks"><?php echo __('Events'); ?></label></td>
+				<td class="field">
+				<input type="checkbox" name="clientanalyticslinks" id="clientanalyticslinks" value="on" class="checkbox"<?php if($clientanalyticslinks == "on"){echo " checked";}?>/>
+				</td>
+				<td class="help"><?php echo __('Additionally track events');?></td>
+			</tr>
+			<tr>
+				<td class="label"><label for="clientanalyticsnoscript"><?php echo __('Non-Javascript'); ?></label></td>
+				<td class="field">
+				<input type="checkbox" name="clientanalyticsnoscript" id="clientanalyticsnoscript" value="on" class="checkbox"<?php if($clientanalyticsnoscript == "on"){echo " checked";}?>/>
+				</td>
+				<td class="help"><?php echo __('Additionally track non-javascript');?></td>
+			</tr>
+			<tr>
+				<td class="label"><label for="clientanalyticsscreenstats"><?php echo __('Screen Stats'); ?></label></td>
+				<td class="field">
+				<input type="checkbox" name="clientanalyticsscreenstats" id="clientanalyticsscreenstats" value="on" class="checkbox"<?php if($clientanalyticsscreenstats == "on"){echo " checked";}?>/>
+				</td>
+				<td class="help"><?php echo __('Track screen dimensions and orientation');?></td>
+			</tr>
+			<tr>
+				<td class="label"><label for="clientanalytics"><?php echo __('Analytics'); ?></label></td>
+				<td class="field">
+				<input name="clientanalytics" id="clientanalytics" value="<?php echo $clientanalytics; ?>" />
+				</td>
+				<td class="help"><?php echo __('Google <a href="https://www.google.com/analytics/settings/home" target="_blank">code or script</a>. Leave blank to disable.');?></td>
+			</tr>
+			<tr>
+				<td class="label"><label for="clientlocation"><?php echo __('Location'); ?></label></td>
+				<td class="field">
+				<input name="clientlocation" id="clientlocation" value="<?php echo $clientlocation; ?>" />
+				</td>
+				<td class="help"><?php echo __('Client or company city.');?></td>
+			</tr>
+
+
+			<tr>
+				<td class="label"><label for="clientanalyticssubdomain"><?php echo __('Subdomains'); ?></label></td>
+				<td class="field">
+				<input type="checkbox" name="clientanalyticssubdomain" id="clientanalyticssubdomain" value="on" class="checkbox"<?php if($clientanalyticssubdomain == "on"){echo " checked";}?>/>
+				</td>
+				<td class="help"><?php echo __('<a href="https://www.google.com/analytics/web/#management/Accounts/">Manage profile</a>');?></td>
+				<!-- http://www.ericmobley.net/guide-to-tracking-multiple-subdomains-in-google-analytics/ -->
+			</tr>
+
+
+
+			<tr>
+				<td class="label"><label for="clientanalyticspolicy"><?php echo __('Privacy policy'); ?></label></td>
+				<td class="field">
+				<textarea name="clientanalyticspolicy" id="clientanalyticspolicy"><?php echo $clientanalyticspolicy; ?></textarea>
+				</td>
+				<td class="help"><?php echo __('Google Analytics policy.');?></td>
+			</tr>
+
+		</table>
+	</fieldset>
+
+
+	<fieldset style="padding: 0.5em;">
 		<legend style="padding: 0em 0.5em 0em 0.5em; font-weight: bold;"><?php echo __('Menu links'); ?></legend>
 		<table class="fieldset" cellpadding="0" cellspacing="0" border="0">
 			<tr>
@@ -154,113 +261,6 @@ $clientanalyticsversion = Plugin::getSetting('clientanalyticsversion', 'seobox')
 		</table>
 	</fieldset>
 	-->
-
-
-	<fieldset style="padding: 0.5em;">
-		<legend style="padding: 0em 0.5em 0em 0.5em; font-weight: bold;"><?php echo __('Search Engines'); ?></legend>
-		<table class="fieldset" cellpadding="0" cellspacing="0" border="0">
-			<tr>
-				<td class="label"><label for="bots"><?php echo __('Robots'); ?></label></td>
-				<td class="field">
-				<input type="checkbox" name="bots" id="bots" value="allow" class="checkbox"<?php if($bots == "allow"){echo " checked";}?>/>
-				</td>
-				<td class="help"><?php echo __('Allow or Disallow robots to index pages. Unchecked when in test mode (default).');?></td>
-			</tr>
-		</table>
-	</fieldset>
-
-
-	<fieldset style="padding: 0.5em;">
-		<legend style="padding: 0em 0.5em 0em 0.5em; font-weight: bold;"><?php echo __('Analytics'); ?></legend>
-		<table class="fieldset" cellpadding="0" cellspacing="0" border="0">
-			<tr>
-				<td class="label"><label for="clientanalyticsstatus"><?php echo __('Enable'); ?></label></td>
-				<td class="field">
-				<input type="checkbox" name="clientanalyticsstatus" id="clientanalyticsstatus" value="on" class="checkbox"<?php if($clientanalyticsstatus == "on"){echo " checked";}?>/>
-				</td>
-				<td class="help"><?php echo __('Yes or No');?></td>
-			</tr>
-
-			<tr>
-				<td class="label"><label for="clientanalyticsversion"><?php echo __('Version'); ?></label></td>
-				<td class="field">
-				<select name="clientanalyticsversion" id="clientanalyticsversion">
-				<?php
-				$clientanalyticsversion_array = array(
-				array ('Classic', 'classic'),
-				array ('Universal (NEW)', 'universal'));
-				foreach($clientanalyticsversion_array as $subarray) {
-					list($text, $val) = $subarray;
-					if($val == $clientanalyticsversion){
-						echo "<option value=\"".str_replace('"',"'",$val)."\" selected>$text</option>";
-					} else {
-						echo "<option value=\"".str_replace('"',"'",$val)."\">$text</option>";
-					}
-				}
-				?>
-				</select>
-				</td>
-				<td class="help"><?php echo __('Analytics method.');?></td>
-			</tr>
-
-			<tr>
-				<td class="label"><label for="clientanalyticslinks"><?php echo __('Events'); ?></label></td>
-				<td class="field">
-				<input type="checkbox" name="clientanalyticslinks" id="clientanalyticslinks" value="on" class="checkbox"<?php if($clientanalyticslinks == "on"){echo " checked";}?>/>
-				</td>
-				<td class="help"><?php echo __('Additionally track events');?></td>
-			</tr>
-			<tr>
-				<td class="label"><label for="clientanalyticsnoscript"><?php echo __('Non-Javascript'); ?></label></td>
-				<td class="field">
-				<input type="checkbox" name="clientanalyticsnoscript" id="clientanalyticsnoscript" value="on" class="checkbox"<?php if($clientanalyticsnoscript == "on"){echo " checked";}?>/>
-				</td>
-				<td class="help"><?php echo __('Additionally track non-javascript');?></td>
-			</tr>
-			<tr>
-				<td class="label"><label for="clientanalyticsscreenstats"><?php echo __('Screen Stats'); ?></label></td>
-				<td class="field">
-				<input type="checkbox" name="clientanalyticsscreenstats" id="clientanalyticsscreenstats" value="on" class="checkbox"<?php if($clientanalyticsscreenstats == "on"){echo " checked";}?>/>
-				</td>
-				<td class="help"><?php echo __('Track screen dimensions and orientation');?></td>
-			</tr>
-			<tr>
-				<td class="label"><label for="clientanalytics"><?php echo __('Analytics'); ?></label></td>
-				<td class="field">
-				<input name="clientanalytics" id="clientanalytics" value="<?php echo $clientanalytics; ?>" />
-				</td>
-				<td class="help"><?php echo __('Google <a href="https://www.google.com/analytics/settings/home" target="_blank">code or script</a>. Leave blank to disable.');?></td>
-			</tr>
-			<tr>
-				<td class="label"><label for="clientlocation"><?php echo __('Location'); ?></label></td>
-				<td class="field">
-				<input name="clientlocation" id="clientlocation" value="<?php echo $clientlocation; ?>" />
-				</td>
-				<td class="help"><?php echo __('Client or company city.');?></td>
-			</tr>
-
-
-			<tr>
-				<td class="label"><label for="clientanalyticssubdomain"><?php echo __('Subdomains'); ?></label></td>
-				<td class="field">
-				<input type="checkbox" name="clientanalyticssubdomain" id="clientanalyticssubdomain" value="on" class="checkbox"<?php if($clientanalyticssubdomain == "on"){echo " checked";}?>/>
-				</td>
-				<td class="help"><?php echo __('<a href="https://www.google.com/analytics/web/#management/Accounts/">Manage profile</a>');?></td>
-				<!-- http://www.ericmobley.net/guide-to-tracking-multiple-subdomains-in-google-analytics/ -->
-			</tr>
-
-
-
-			<tr>
-				<td class="label"><label for="clientanalyticspolicy"><?php echo __('Privacy policy'); ?></label></td>
-				<td class="field">
-				<textarea name="clientanalyticspolicy" id="clientanalyticspolicy"><?php echo $clientanalyticspolicy; ?></textarea>
-				</td>
-				<td class="help"><?php echo __('Google Analytics policy.');?></td>
-			</tr>
-
-		</table>
-	</fieldset>
 
 
 	<p class="buttons">
