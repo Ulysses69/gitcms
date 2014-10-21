143b00bb0b9e7777609613ae96197ead2396ee39
Ulysses69 <steven@poppymedia.co.uk>
2014-10-21 11:12:31 +0100

    Added language file support.

---
+++ wolf/plugins/social/i18n/en-message.php
@@ -0,0 +1,30 @@
+<?php
+
+	/**
+	 * Czech file for plugin comment
+	 *
+	 * @package Plugins
+	 * @subpackage comment
+	 *
+	 * @author Iva Pelcová <ivy046@gmail.com>
+	 * @version Wolf 0.6.0
+	 */
+
+	return array(
+	'Social - unable to access plugin settings.' => 'Social - unable to access plugin settings.',
+	'Social - plugin settings updated.' => 'Social - plugin settings updated.',
+	'Social Icons' => 'Social Icons',
+	'Display' => 'Display',
+	'Appearance of social links' => 'Appearance of social links',
+	'Icon Set' => 'Icon Set',
+	'Appearance' => 'Appearance',
+	'Social Links' => 'Social Links',
+	'Social' => 'Social',
+	'Save' => 'Save',
+	'Cancel' => 'Cancel',
+	'Save then close' => 'Save then close',
+	'Close without saving' => 'Close without saving',
+	'unable to retrieve plugin settings.' => 'unable to retrieve plugin settings.',
+	'plugin settings saved.' => 'plugin settings saved.',
+	'plugin settings not saved!' => 'plugin settings not saved!',
+	);
