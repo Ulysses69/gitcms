d3777ac4226d222b4a500f7fb56d42914834cfad
Ulysses69 <steven@poppymedia.co.uk>
2015-02-11 09:33:05 +0000

    Updated notes.

--- wolf/plugins/_htaccess/views/sidebar.php
+++ wolf/plugins/_htaccess/views/sidebar.php
@@ -2,3 +2,7 @@
 <h2>Warning</h2>
 <p>Server configuration changes should not be carried out without server knowledge. Incorrect configuration can result in this website being broken and this admin area being unreachable.</p>
 </div>
+<div class="box warning">
+<h2>Note</h2>
+<p>Changes made here are for quick fixes only. Changes to further admin settings will overwite changes made here.</p>
+</div>
